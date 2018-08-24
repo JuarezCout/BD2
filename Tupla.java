3f63e452c18669db85fec65bed1b316e1aac7c50
JuarezCout <juarezcout15.jl@gmail.com>
2018-08-23 23:10:41 -0300

    Add files via upload

---
+++ Tupla.java
@@ -0,0 +1,16 @@
+package estruturas;
+
+import java.util.HashMap;
+
+public class Tupla {
+	
+	HashMap<Integer, String> tipoDados;
+	HashMap<Integer, String> tupla;
+	byte[] dadosByte;
+	
+	public Tupla () {
+		tipoDados = new HashMap<Integer, String>();
+		tupla     = new HashMap<Integer, String>();
+	}
+
+}
