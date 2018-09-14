2a79bf64cc12714cbc503dd9e254ea180356ec6c
Juarez Coutinho <juarezcout15.jl@gmail.com>
2018-09-13 22:59:24 -0300

    Changes: Buffer e Geração de Arquivo

--- App.java
+++ App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         Leitor leitor = new Leitor();
         leitor.iniciarLeitura();
-        Buffer.geraRequisicoes(leitor.container);
+        GerenciadorBuffer.geraRequisicoes(leitor.container);
     }
 }
